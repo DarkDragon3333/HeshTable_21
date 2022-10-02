650bf89213c9e8341e98bbd6f66f938be01639de
Максим Минайчев <minaichiev-dragon2017@yandex.ru>
2022-10-02 22:33:44 +0300

    Добавьте файлы проекта.

---
+++ Хеш-таблица вариант 21/HeshTable.cpp
@@ -0,0 +1,1 @@
+#include "HeshTable.h"
---
+++ Хеш-таблица вариант 21/Хеш-таблица вариант 21.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include "HeshTable.h"
+
+using namespace std;
+
+int c = 0;
+int arr[4] = {145, 134, 78, 65};
+
+//Грубая реализация
+string HeshFunction(string x) {
+
+	string arr1[] { "apple", "plum", "tomato", "potato" };
+
+	for (size_t i = 0; i < 4; i++) {
+		c = c + 1;
+		if (x == arr1[i]) {
+			string s = to_string(arr[i]);
+			return s;
+		}
+		else if (c == 4) {
+			string s = "Don't find";
+			return s; 
+		}
+	}   
+}
+
+
+int main()
+{
+	
+	return 0;
+}
